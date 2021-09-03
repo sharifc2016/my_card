c9167e0b3914f94c4c9c1cec1f56af0c796317a2
sharifc2016 <sharifc2016@gmail.com>
2021-09-03 08:56:47 +0600

    Add name and designation

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,24 @@ class MyApp extends StatelessWidget {
                 radius: 50,
                 backgroundImage: AssetImage('images/my_image.png'),
               ),
+              Text(
+                "Sharif Chowdhury",
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "SOFTWARE ENGINEER",
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ],
           ),
         ),
